7a201ee1e4d9b1cc9de46da9f5a305bdd2e1b98f
sbs20 <sam.strachan@gmail.com>
2016-01-15 16:34:38 +0000

    Quick variable name refactor

--- app/src/main/java/sbs20/filenotes/NoteArrayAdapter.java
+++ app/src/main/java/sbs20/filenotes/NoteArrayAdapter.java
@@ -6,12 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.text.DateFormat;
-import java.text.Format;
-import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 public class NoteArrayAdapter extends GenericBaseAdpater<Note> {
 
@@ -33,22 +29,22 @@ public class NoteArrayAdapter extends GenericBaseAdpater<Note> {
             row = inflater.inflate(R.layout.listview_notes, parent, false);
         }
 
-        TextView filename = (TextView) row.findViewById(R.id.listnote_name);
-        filename.setText(note.getName());
+        TextView name = (TextView) row.findViewById(R.id.listnote_name);
+        name.setText(note.getName());
 
-        TextView textNoteText = (TextView) row.findViewById(R.id.listnote_text);
-        textNoteText.setText(note.getTextSummary());
+        TextView text = (TextView) row.findViewById(R.id.listnote_text);
+        text.setText(note.getTextSummary());
 
-        TextView modificationDate = (TextView) row.findViewById(R.id.listnode_lastModified);
+        TextView lastModified = (TextView) row.findViewById(R.id.listnode_lastModified);
         MainActivity activity = (MainActivity)this.context;
         DateTimeHelper dateTimeHelper = activity.getFilenotesApplication().getDateTimeHelper();
 
         String date = dateTimeHelper.formatDate(note.getLastModified());
         date += "\n" + dateTimeHelper.formatTime(note.getLastModified());
-        modificationDate.setText(date);
+        lastModified.setText(date);
 
-        TextView fileSize = (TextView) row.findViewById(R.id.listnote_size);
-        fileSize.setText(note.getSizeString());
+        TextView size = (TextView) row.findViewById(R.id.listnote_size);
+        size.setText(note.getSizeString());
 
         row.setTag(note);
 
